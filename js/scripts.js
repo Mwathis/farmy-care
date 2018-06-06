4e17a3649e7aa7d3748acfbea44a839ff03102d5
Charles Mwathi <cmwathi24@gmail.com>
2018-06-06 19:57:01 +0300

    Add more scripts

--- js/scripts.js
+++ js/scripts.js
@@ -26,15 +26,15 @@ $(document).ready(function() {
 });
 
 function show(data) {
-  return    "<h2 style='font-size:40px;font-weight:bold;'>Current Weather for "+data.name+","+data.sys.country+"</h2>"+
-            "<h3><strong>Weather</strong>:<img src='http://openweathermap.org/img/w/"+data.weather[0].icon+".png'> "+ data.weather[0].main +"</h3>" +
-            "<h3><strong>Description</strong>: "+ data.weather[0].description +"</h3>"+
-            "<h3><strong>Temperature</strong>: "+ data.main.temp +"&deg;C</h3>"+
-            "<h3><strong>Pressure</strong>: "+ data.main.pressure +" hPa</h3>"+
-            "<h3><strong>Humidity</strong>: "+ data.main.humidity +"%</h3>"+
-            "<h3><strong>Min. Temperature</strong>: "+ data.main.temp_min +"&deg;C</h3>"+
-            "<h3><strong>Max. Temperature</strong>: "+ data.main.temp_max +"&deg;C</h3>"+
-            "<h3><strong>Wind Speed</strong>: "+ data.wind.speed +" m/s</h3>"+
-            "<h3><strong>Wind Direction</strong>: "+ data.wind.deg +"&deg;</h3>";
+  return    "<h2 style='font-size:40px;font-weight:bold;'class='text-center'>Current Weather for "+data.name+","+data.sys.country+"</h2>"+
+            "<h3 style='padding-left:485px;'><strong>Weather</strong>:<img src='http://openweathermap.org/img/w/"+data.weather[0].icon+".png'> "+ data.weather[0].main +"</h3>" +
+            "<h3 style='padding-left:485px;'><strong>Description</strong>: "+ data.weather[0].description +"</h3>"+
+            "<h3 style='padding-left:485px;'><strong>Temperature</strong>: "+ data.main.temp +"&deg;C</h3>"+
+            "<h3 style='padding-left:485px;'><strong>Pressure</strong>: "+ data.main.pressure +" hPa</h3>"+
+            "<h3 style='padding-left:485px;'><strong>Humidity</strong>: "+ data.main.humidity +"%</h3>"+
+            "<h3 style='padding-left:485px;'><strong>Min. Temperature</strong>: "+ data.main.temp_min +"&deg;C</h3>"+
+            "<h3 style='padding-left:485px;'><strong>Max. Temperature</strong>: "+ data.main.temp_max +"&deg;C</h3>"+
+            "<h3 style='padding-left:485px;'><strong>Wind Speed</strong>: "+ data.wind.speed +" m/s</h3>"+
+            "<h3 style='padding-left:485px;'><strong>Wind Direction</strong>: "+ data.wind.deg +"&deg;</h3>";
 
 }
